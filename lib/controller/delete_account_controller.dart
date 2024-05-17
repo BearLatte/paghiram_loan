4cd5f3478e26eb53d853f2616e4e9803ca16875f
Tim <TOPshuaiyeai@163.com>
2024-05-17 10:06:02 +0800

    添加了alert 的弹窗

--- lib/controller/delete_account_controller.dart
+++ lib/controller/delete_account_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:paghiram_loan/common/common_alert.dart';
+import 'package:paghiram_loan/common/common_image.dart';
 import 'package:paghiram_loan/service/index.dart';
 import 'package:paghiram_loan/util/constant.dart';
 import 'package:paghiram_loan/util/global.dart';
@@ -42,7 +43,7 @@ class DeleteAccountController extends GetxController {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Stack(children: [
+                Stack(clipBehavior: Clip.none, alignment: Alignment.center, children: [
                   Container(
                     padding: EdgeInsets.fromLTRB(12, 64, 12, 24),
                     decoration: BoxDecoration(
@@ -104,6 +105,14 @@ class DeleteAccountController extends GetxController {
                         )
                       ],
                     ),
+                  ),
+                  Positioned(
+                    top: -40,
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      child: CommonImage(src: 'asset/icons/lock_icon.png'),
+                    ),
                   )
                 ])
               ],
@@ -119,32 +128,42 @@ class DeleteAccountController extends GetxController {
           return Dialog(
               backgroundColor: Colors.transparent,
               child: Column(mainAxisSize: MainAxisSize.min, children: [
-                Stack(children: [
-                  Container(
-                    padding: EdgeInsets.fromLTRB(16, 64, 16, 24),
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(colors: [HexColor('#FFEBF8F9'), Colors.white], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(children: [
-                      Text('Account Deleted Successfully', style: TextStyle(color: HexColor('#FF06A77D'), fontSize: 18, fontWeight: FontWeight.w600)),
-                      SizedBox(height: 16),
-                      Text('The current account has been deleted. You can re-register or log in with an existing account',
-                          style: TextStyle(color: HexColor('#FF102729'), fontSize: 15, fontWeight: FontWeight.w600)),
-                      SizedBox(height: 24),
-                      TextButton(
+                Stack(
+                  clipBehavior: Clip.none,
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(16, 64, 16, 24),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [HexColor('#FFEBF8F9'), Colors.white], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(children: [
+                        Text('Account Deleted Successfully', style: TextStyle(color: HexColor('#FF06A77D'), fontSize: 18, fontWeight: FontWeight.w600)),
+                        SizedBox(height: 16),
+                        Text('The current account has been deleted. You can re-register or log in with an existing account',
+                            style: TextStyle(color: HexColor('#FF102729'), fontSize: 15, fontWeight: FontWeight.w600)),
+                        SizedBox(height: 24),
+                        TextButton(
                           onPressed: () {
                             Global.clearLoginInfo();
                             Get.until((route) => route.isFirst);
                           },
                           child: Container(
-                              width: 200,
-                              height: 40,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(color: HexColor('#FF06A77D'), borderRadius: BorderRadius.circular(100)),
-                              child: Text('OK', style: TextStyle(color: Colors.white))))
-                    ]),
-                  )
-                ])
+                            width: 200,
+                            height: 40,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(color: HexColor('#FF06A77D'), borderRadius: BorderRadius.circular(100)),
+                            child: Text(
+                              'OK',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ]),
+                    ),
+                    Positioned(top: -40, child: Container(width: 80, height: 80, child: CommonImage(src: 'asset/icons/successful_icon.png')))
+                  ],
+                )
               ]));
         });
   }
